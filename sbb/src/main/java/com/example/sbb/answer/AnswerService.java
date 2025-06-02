8d21435ddbbc23e3afa3e42ec281813a926c3487
hyunwoo4073 <qogusdn13@gmail.com>
2025-06-02 16:17:14 +0900

    Springboot_v6 질문, 답변 수정/삭제 기능 추가

--- sbb/src/main/java/com/example/sbb/answer/AnswerService.java
+++ sbb/src/main/java/com/example/sbb/answer/AnswerService.java
@@ -1,7 +1,9 @@
 package com.example.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import com.example.sbb.DataNotFoundException;
 import com.example.sbb.question.Question;
 import com.example.sbb.user.SiteUser;
 
@@ -21,5 +23,24 @@ public class AnswerService {
     answer.setQuestion(question);
     answer.setAuthor(author);
     this.answerRepository.save(answer);
+   }
+
+   public Answer getAnswer(Integer id) {
+      Optional<Answer> answer = this.answerRepository.findById(id);
+      if (answer.isPresent()) {
+         return answer.get();
+      } else {
+         throw new DataNotFoundException("answer not found");
+      }
+   }
+
+   public void modify(Answer answer, String content) {
+      answer.setContent(content);
+      answer.setModifyDate(LocalDateTime.now());
+      this.answerRepository.save(answer);
+   }
+
+   public void delete(Answer answer) {
+      this.answerRepository.delete(answer);
    }
 }
--- sbb/src/main/java/com/example/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/example/sbb/question/QuestionController.java
@@ -6,6 +6,8 @@ import java.security.Principal;
 import com.example.sbb.user.SiteUser;
 import com.example.sbb.user.UserService;
 
+import groovyjarjarantlr4.v4.parse.ANTLRParser.throwsSpec_return;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -82,4 +86,42 @@ public class QuestionController {
         return "redirect:/question/list"; // 질문 저장 후 질문 목록으로 이동
     }
     
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id
+    , Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String questionModify(@Valid QuestionForm questionForm, BindingResult bindingResult,
+    Principal principal, @PathVariable("id") Integer id) {
+        if (bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
+        }
+        this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
+        return String.format("redirect:/question/detail/%s", id);
+    }
+    
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/";
+    }
 }
