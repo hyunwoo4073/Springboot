060499f59993dfe9c0320a52dc40ec6e126e6b3d
hyunwoo4073 <qogusdn13@gmail.com>
2025-06-05 16:15:55 +0900

    Springboot_v9 nginx 사용

--- sbb/start.sh
+++ sbb/start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # 배포파일 입력
-JAR=/home/bae/sbb/target/sbb-0.0.2-SNAPSHOT.jar
+JAR=/home/bae/sbb/target/sbb-0.0.3-SNAPSHOT.jar
 # 출력할 로그파일 입력력
-LOG=/home/bae/sbb/sbb.log
+# LOG=/home/bae/sbb/sbb.log
 
-java -Dspring.profiles.active=prod -jar $JAR > $LOG 2>&1 &
+# /dev/null로 지정하면 콘솔출력이 무시됨
+# 로깅 설정을 했기 때문에 변경
+LOG=/dev/null
+export spring_profiles_active=prod
+
+
+# java -Dspring.profiles.active=prod -jar $JAR > $LOG 2>&1 &
+java -jar $JAR > $LOG 2>&1 &
