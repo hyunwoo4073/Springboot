27295b1cad46876ff46029164ce94d7d4b8c91e0
hyunwoo4073 <qogusdn13@gmail.com>
2025-06-05 13:33:06 +0900

    스크립트 주석 추가

--- sbb/start.sh
+++ sbb/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-JAR=sbb-0.0.2-SNAPSHOT.jar
+# 배포파일 입력
+JAR=/home/bae/sbb/target/sbb-0.0.2-SNAPSHOT.jar
+# 출력할 로그파일 입력력
 LOG=/home/bae/sbb/sbb.log
 
 java -Dspring.profiles.active=prod -jar $JAR > $LOG 2>&1 &
--- sbb/stop.sh
+++ sbb/stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# 'ps -ef' 명령은 현재 실행 중인 프로세스를 출력
+# 'grep java | grep sbb' 명령은 출력된 문자열에서 문장이 포함된 프로세스만 필터링
+# awk '{pring $2}' 명령은 출력 문자열의 2번째 항목인 프로세스 아이디만 뽑아내는 역할을 함
 SBB_UID=$(ps -ef | grep java | grep sbb | awk '{pring $2}')
 
 if [ -z "$SBB_PID" ];
